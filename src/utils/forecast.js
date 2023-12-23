c9685598bf7fd4fd612f9039d88ded5a7efd9fba
FDB09 <ferdaniel.dev@gmail.com>
2023-12-23 16:16:03 -0600

    Added humidity to forescast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=63e3cede4bab3251466e39297db11c94&query=${latitude},${longitude}`
+    const url = `http://api.weatherstack.com/current?access_key=63e3cede4bab3251466e39297db11c94&query=${latitude},${longitude}&units=m`
     
     request({url, json:true}, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is ${body.current.temperature} degrees out. Feels like ${body.current.feelslike} degrees.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is ${body.current.temperature} degrees out. Feels like ${body.current.feelslike} degrees. Humidity ${body.current.humidity}%.`)
         }
     })
 }
